c97f66d2d2693572f8f465f2f2fb310705e1821a
Maria Pasca <maria13pasca@gmail.com>
2021-05-20 11:40:32 +0300

    final final final

--- queues_simulator/src/main/java/ExceptionIncorrectInput.java
+++ queues_simulator/src/main/java/ExceptionIncorrectInput.java
@@ -1,5 +1,9 @@
+import javax.swing.*;
+
 public class ExceptionIncorrectInput extends Exception{
-    public ExceptionIncorrectInput(String wrongInput, String why){
+    public ExceptionIncorrectInput(String wrongInput, String why, JFrame frame){
         super("Incorrect input: " + wrongInput + "; " + why);
+
+        JOptionPane.showMessageDialog(frame, "Incorrect input: " + wrongInput + "; " + why);
     }
 }
